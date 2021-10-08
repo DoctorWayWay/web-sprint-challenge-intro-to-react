c208d0d23ccdef57c2a6bca97ea9fd337f82d14a
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-10-08 10:01:46 -0700

    Added a media breakpoint in index.css to make the background look nice on smaller screens. Created an axios call to fetch data from the starwars mock API in App.js. Started working on character component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import './App.css';
+// Importing Libraries
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+// Importing Components
+//
+// Importing StyleSheets
+import "./App.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people")
+      .then((response) => {
+        console.log(response);
+        setCharacters(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
     </div>
   );
-}
+};
 
 export default App;
